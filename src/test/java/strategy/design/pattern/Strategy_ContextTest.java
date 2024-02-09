41afc5235382231845d40ef504fa2ccce31b9088
yassine <dbaichiyasine4@gmail.com>
2024-02-09 18:31:34 +0100

    add the test classes of strategy design pattern

--- src/test/java/strategy/design/pattern/Strategy_ContextTest.java
+++ src/test/java/strategy/design/pattern/Strategy_ContextTest.java
@@ -22,12 +22,5 @@ public class Strategy_ContextTest extends TestCase {
         assertEquals(expected, result);
     }
 
-    @Test
-    public void testSubtractionStrategy() {
-        double expected = 1; // Assuming you have a Subtraction strategy
-        Calculation_Strategy subtraction_strategy = new Substraction(); // This should be your actual subtraction strategy class
-        Strategy_Context strategyContext = new Strategy_Context(subtraction_strategy);
-        double result = strategyContext.execute_calculation_strategy(3, 2);
-        assertEquals(expected, result);
-    }
+    
 }
