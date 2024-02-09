ff21db2e7be50d454582fcc35894a2210bc4ba2c
yassine <dbaichiyasine4@gmail.com>
2024-02-09 18:23:48 +0100

    add strategy design pattern class and its associated other classes

--- src/main/java/strategy/design/pattern/Substraction.java
+++
@@ -1,8 +0,0 @@
-package strategy.design.pattern;
-
-public class Substraction implements Calculation_Strategy{
-    @Override
-    public double calculation(double a, double b) {
-        return a-b;
-    }
-}
